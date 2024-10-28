b09a65f56f96e15e434b730a8622050a0ab46e67
vmoskaltsev <vmoskaltsev@gmail.com>
2024-10-28 16:58:10 +0200

    -

---
+++ src/main/java/org/example/PostgreSQLConnection.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PostgreSQLConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/employee_db";
+    private static final String USER = "User";
+    private static final String PASSWORD = "19732846Gs56";
+
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Подключение к базе данных выполнено успешно.");
+        } catch (SQLException e) {
+            System.out.println("Ошибка подключения: " + e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        connect();
+    }
+}

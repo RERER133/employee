d17d913bed7c78a08edfa860bf5abd30e2620ea7
vmoskaltsev <vmoskaltsev@gmail.com>
2024-10-29 16:52:54 +0200

    -f

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,11 @@
 package org.example;
 
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         PostgreSQLConnection postgreSQLConnection = new PostgreSQLConnection();
         postgreSQLConnection.connect();
+        postgreSQLConnection.findEmployeeByName("Jane");
     }
 }
--- src/main/java/org/example/PostgreSQLConnection.java
+++ src/main/java/org/example/PostgreSQLConnection.java
@@ -1,23 +1,36 @@
 package org.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class PostgreSQLConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/employees_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "19732846Gs56";
+    private static final String url = "jdbc:postgresql://localhost:5432/employees_db";
+    private static final String username = "postgres";
+    private static final String password = "19732846Gs56";
 
     public static Connection connect() {
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            conn = DriverManager.getConnection(url, username, password);
             System.out.println("Подключение к базе данных выполнено успешно.");
         } catch (SQLException e) {
             System.out.println("Ошибка подключения: " + e.getMessage());
         }
         return conn;
+    }
+    public void findEmployeeByName(String firstName) throws SQLException {
+        String query = "SELECT * FROM employees WHERE first_name = ?";
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, firstName);
+            ResultSet rs = preparedStatement.executeQuery();
+
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id") + ", Name: "
+                        + rs.getString("first_name") + " " + rs.getString("last_name")
+                        + ", Department: " + rs.getString("department")
+                        + ", Salary: " + rs.getBigDecimal("salary"));
+            }
+        }
     }
 
     public static void main(String[] args) {
